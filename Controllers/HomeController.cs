cb26c46e9ae6d0ad62baa52227986af7c30dd8be
Bùi Quốc Huy <huy01682312920@gmail.com>
2021-11-23 15:27:15 +0700

    done

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,11 +11,11 @@ namespace ThucHanhTuan5.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly Storectx _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(Storectx context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -33,5 +33,56 @@ namespace ThucHanhTuan5.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Cau3()
+        {
+        
+            return View(_context.Cau3());
+        }
+        public IActionResult Cau4()
+        {
+
+            return View(_context.Cau4());
+        }
+        public IActionResult Cau5()
+        {
+
+            return View(_context.Cau5());
+        }
+        public IActionResult Cau6()
+        {
+
+            return View(_context.Cau6());
+        }
+        public IActionResult Cau7()
+        {
+
+            return View(_context.Cau7());
+        }
+        public IActionResult Cau8()
+        {
+
+            return View(_context.Cau8());
+        }
+        virtual public IActionResult Cau9()
+        {
+
+            return View(_context.Cau9());
+        }
+        virtual public IActionResult Cau13()
+        {
+
+            return View(_context.Cau13());
+        }
+        virtual public IActionResult Cau14()
+        {
+
+            return View(_context.Cau14());
+        }
+        virtual public IActionResult Cau17()
+        {
+
+            return View(_context.Cau17());
+        }
+
     }
 }
